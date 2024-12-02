f9646cfc3dfab508c113b3e6a2b0c65c800925d5
izzytoot <isabeltootill@gmail.com>
2024-12-02 19:08:07 +0000

    level1

--- Rank_1/Level_1/rev_print.c
+++ Rank_1/Level_1/rev_print.c
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/02 12:29:22 by root              #+#    #+#             */
-/*   Updated: 2024/12/02 18:00:53 by root             ###   ########.fr       */
+/*   Created: 2024/12/02 18:02:01 by root              #+#    #+#             */
+/*   Updated: 2024/12/02 18:15:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(int ac, char **av)
+char	*rev_print(char *str)
 {
 	int	i = 0;
-	int	count = 1;
-
-	if (ac == 2)
-	{
-		while (av[1][i])
-		{
-			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				count = av[1][i] - 'a' + 1;
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				count = av[1][i] - 'A' + 1;
-			while (count--)
-				write (1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+	
+	while (str[i])
+		i++;
+	while (i--)
+		write (1, &str[i], 1);
+	write (1, "\n", 1);
+	return (str);
 }
 
+/*
+int main(void)
+{
+	rev_print("Hello world");
+	write (1, "\n", 1);
+	rev_print("tnirp esreveR");
+	write (1, "\n", 1);
+	rev_print("");
+	write (1, "\n", 1);
+	return (0);
+}
+*/
