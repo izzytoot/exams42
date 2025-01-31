56328f4b824586ee0d12ad7ca04dc2cf4449231d
izzytoot <isabeltootill@gmail.com>
2025-01-31 18:43:25 +0000

    ggg

---
+++ Rank_3/printf/my_printf.c
@@ -0,0 +1,20 @@
+
+int ft_printf(const char *spec, ... );
+{
+	va_list(args);
+	int	i = 0;
+	int	len = 0;
+
+	va_start(args, spec);
+	{
+		while (spec[i])
+		{
+			/* code */
+		}
+		
+	}
+	va_arg
+
+	va_end
+
+}
