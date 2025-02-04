a0dff709eacbaba8bd1e3753bda43840db68abd3
Isabel Cunha Tootill <icunha-t@c2r3s10.42porto.com>
2025-02-04 18:21:30 +0000

    final

---
+++ Rank_3/get_next_line/get_next_line.c
@@ -0,0 +1,96 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#endif
+#endif
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+char	*join_char_to_line(char *str, char c, int len)
+{
+	char *new_str;
+	int i = 0;
+
+	new_str = (char *)malloc(len + 2);
+	if (!new_str)
+		return (NULL);
+	while (i < len)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i++] = c;
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char buffer[BUFFER_SIZE];
+	static int buffer_real_size = 0;
+	static int buffer_index = 0;
+	char *new_line;
+	int	i = 0;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return(NULL);
+	new_line = (char *)malloc(1);
+	if (!new_line)
+		return(NULL);
+	new_line[0] = '\0';
+	while(1)
+	{
+		if (buffer_real_size <= buffer_index)
+		{
+			buffer_real_size = read(fd, buffer, BUFFER_SIZE);
+			buffer_index = 0;
+			if (buffer_real_size <= 0)
+			{
+				if (i == 0)
+				{
+					free(new_line);
+					return (NULL);
+				}
+			}
+			break;
+		}
+		new_line = join_char_to_line(new_line, buffer[buffer_index++], i);
+		if (!new_line)
+			return (NULL);
+		if(new_line[i++] == '\n')
+			break;
+	}
+	return (new_line);
+}
+/*
+#include <stdio.h>
+int main(int ac, char **av)
+{
+	char *line;
+	char *path = av[1];
+	int fd = open(path, O_RDONLY);
+
+	(void)ac;
+	if (fd < 0)
+	{
+		printf("Error\n");
+		return(0);
+	}
+	while((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return(0);
+}
+*/
---
+++ Rank_3/printf/ft_printf.c
@@ -0,0 +1,89 @@
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int ft_putstr(char *str)
+{
+	int	len = 0;
+	int i = 0;
+
+	if (!str)
+		str = "(null)";
+	while (str[i])
+	{
+		len += write(1, &str[i], 1);
+		i++;
+	}
+	return (len);
+}
+
+int ft_putnbr(int nb, int base)
+{
+	int	len = 0;
+	unsigned int temp_nb = 0;
+	char *hexa = "0123456789abcdef";
+
+	if (nb < 0 && base == 10)
+	{
+		len += write(1, "-" , 1);
+		temp_nb = -nb;
+	}
+	else
+		temp_nb = (unsigned int)nb;
+	if (temp_nb >= (unsigned int)base)
+		len += ft_putnbr((temp_nb / base), base);
+	len += write(1, &hexa[temp_nb % base], 1);
+	return (len);
+}
+
+int ft_printf(const char *mandatory, ... )
+{
+	va_list(args);
+	int	i = 0;
+	int	len = 0;
+
+	va_start(args, mandatory);
+	while(mandatory[i])
+	{
+		if (mandatory[i] == '%' && mandatory[i + 1] != '\0')
+		{
+			i++;
+			if (mandatory[i] == 's')
+				len += ft_putstr(va_arg(args, char *));
+			else if (mandatory[i] == 'd')
+				len += ft_putnbr(va_arg(args, int), 10);
+			else if (mandatory[i] == 'x')
+				len += ft_putnbr(va_arg(args, int), 16);
+			else if (mandatory[i] == '%')
+				len += write(1, "%", 1);
+		}
+		else
+			len += write(1, &mandatory[i], 1);	
+		i++;
+	}
+	return (len);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int mycount = 0;
+	int origcount = 0;
+
+	mycount = ft_printf("%s\n", "toto");
+	origcount = printf("%s\n", "toto");
+	ft_printf("mycount: %d\n", mycount);
+	printf("origcount: %d\n", origcount);
+	mycount = ft_printf("Magic %s is %d\n", "number", 42);
+	origcount = printf("Magic %s is %d\n", "number", 42);
+	ft_printf("mycount: %d\n", mycount);
+	printf("origcount: %d\n", origcount);
+	mycount = ft_printf("Hexadecimal for %d is %x\n", 42, -42);
+	origcount = printf("Hexadecimal for %d is %x\n", 42, -42);
+	ft_printf("mycount: %d\n", mycount);
+	printf("origcount: %d\n", origcount);
+
+	return (0);
+}
+*/
